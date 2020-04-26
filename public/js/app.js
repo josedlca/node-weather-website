dba2a4f9980a285f4ae857f850dfd5756cb65cff
josedlca <jose.dlcruz.ayala@gmail.com>
2020-04-26 17:50:24 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,15 +9,11 @@ weatherForm.addEventListener('submit',(e)=>{
 
     messageOne.textContent = "Loading..."
     messageTwo.textContent = ''
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+    fetch(`/weather?address=${location}`).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
-            console.log(data.error)
             messageOne.textContent = data.error
         }else{
-            console.log(data.forecast)
-            console.log(data.location)
-            console.log(data.address)
             messageOne.textContent = data.forecast
             messageTwo.textContent = data.location
         }
